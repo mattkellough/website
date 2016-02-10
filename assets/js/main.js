325d44ad94a01629c4a836e41e1f7ee400fd9be7
Brian Kim <bkim8815@gmail.com>
2016-02-10 10:51:01 -0500

    js file modified

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,16 +8,6 @@
 
 	skel
 		.breakpoints({
-			xlarge:	'(max-width: 1680px)',
-			large:	'(max-width: 1280px)',
-			medium:	'(max-width: 980px)',
-			small:	'(max-width: 736px)',
-			xsmall:	'(max-width: 480px)'
-		});
-
-	$(function() {
-
-		var	$window = $(window),
 			$body = $('body'),
 			$wrapper = $('#page-wrapper'),
 			$banner = $('#banner'),
