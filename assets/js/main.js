2fa1e2576d0a8c862e85445b6bdf175ae0b143cc
Matt Kellough <mrk10a@gmail.com>
2016-02-10 10:57:33 -0500

    Begin the conflict

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,7 +8,7 @@
 
 	skel
 		.breakpoints({
-			xlarge:	'(max-width: 1680px)',
+			xlarge:	'(max-width: 1680px)', Brians a bitch
 			large:	'(max-width: 1280px)',
 			medium:	'(max-width: 980px)',
 			small:	'(max-width: 736px)',
